689747a55520e0d0393e3d75175f71d795399667
Neo <fullstackneo@gmail.com>
2022-05-25 22:21:40 -0600

    optimize js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path');
 const webpack = require('webpack');
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = {
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
     }),
+    new BundleAnalyzerPlugin({
+      // the report outputs to an HTML file in the dist folder
+      analyzerMode: 'static',
+    }),
   ],
-  entry: './assets/js/script.js',
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js',
+  },
   output: {
+    filename: '[name].bundle.js',
     path: path.join(__dirname, 'dist'),
-    filename: 'main.bundle.js',
   },
   mode: 'development',
 };
